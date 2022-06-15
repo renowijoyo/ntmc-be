bed2a983fc9597bb77ff3666f6d0af3c45a7f45e
Reno Wijoyo <renowijoyo@gmail.com>
2022-06-15 15:02:17 +0700

    fix cred

--- dbconfig2.py
+++ dbconfig2.py
@@ -13,8 +13,8 @@ class DBConfig2:
 
         dbcon = mysql.connect(
             host="202.67.14.247",
-            user="ntmc_ccntmc",
-            passwd="0uH7kc6ceEYt",
+            user="ntmc_brimob_id",
+            passwd="brimob123!",
             database="ntmc_brimob_id"
         )
 
