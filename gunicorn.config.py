796beab9afe33755d5c8213d3a8f3fa06a65157c
Reno Wijoyo <renowijoyo@gmail.com>
2022-08-01 19:24:44 +0700

    add gunicorn config

---
+++ gunicorn.config.py
@@ -0,0 +1,8 @@
+bind = "0.0.0.0:5000"
+workers = 4
+pidfile = 'pidfile'
+errorlog = 'errorlog'
+loglevel = 'info'
+accesslog = 'accesslog'
+# certfile = '/etc/letsencrypt/live/api.brimob.id/fullchain.pem'
+# keyfile = '/etc/letsencrypt/live/api.brimob.id/privkey.pem'
