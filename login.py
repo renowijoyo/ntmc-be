12cacd36c82c8e7cff9740b074906395619080ea
Reno Wijoyo <renowijoyo@gmail.com>
2022-08-04 10:00:22 +0700

    set password

--- login.py
+++ login.py
@@ -36,6 +36,26 @@ def get_db():
         g.mysql_db = dbObj.connect()
     return g.mysql_db
 
+@login_blueprint.route('/password_update', methods=["POST"])
+def password_update():
+    username = request.form['username']
+    password = request.form['password']
+    # ol_password = request.json.get('ol_password')
+
+    cursor = db.cursor(dictionary=True)
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+
+    query = "UPDATE user SET password =  %s WHERE username= %s"
+    cursor.execute(query, (hashed,username,))
+    db.commit()
+
+    res = dict()
+    res['rowcount'] = cursor.rowcount
+    cursor.close()
+    return res
+
+
 
 @login_blueprint.route('/check_login', methods=["POST"])
 def check_login():
@@ -44,11 +64,26 @@ def check_login():
     result = dict()
     username = request.form['username']
     password = request.form['password']
+    valid = 0
     query = "SELECT iduser,username,password,level_user,satwil_id,satwil,polda,polda.idpolda as polda_id, user_license.status AS 'status_license' FROM user " \
             "LEFT JOIN satwil ON satwil.idsatwil = user.satwil_id LEFT JOIN polda ON polda.idpolda = user.polda_id LEFT JOIN user_license ON " \
             "user.iduser = user_license.user_id WHERE username = %s"
     cursor.execute(query, (username,))
     records = cursor.fetchall()
-    records[0]['login_success'] = 'true'
+    if (len(records) > 0):
+        # salt = bcrypt.gensalt()
+        # hashed = bcrypt.hashpw(password.encode(), salt)
+        try:
+            if bcrypt.checkpw(password.encode(), (records[0]['password']).encode()):
+                valid = 1
+            else:
+                valid = 0
+        except:
+            valid = 0
+        records[0]['login_success'] = valid
+    else:
+        valid = 0
+
+
     result = records
     return jsonify(result)
